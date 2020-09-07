45a91cf62d83bcc4b14f2c1c8575e9d8bf3c61e1
pinakinc <chaubalpinakin@gmail.com>
2020-09-07 17:25:31 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -4,18 +4,27 @@ import org.openqa.selenium.By;
 
 import singletondriver.SingletonDriver;
 
-public class LoginPage {
+public class LoginPage extends AbstractBasePage{
 	By email = By.name("email");
 	By password = By.name("password");
 	By btnLogin = By.xpath("//div[text()='Login']");
 	public void enterUserName(String userName) {
 		SingletonDriver.getInstance().getDrvr().findElement(email).sendKeys(userName);
 	}
-	public void enterPassword(String password) {
-		SingletonDriver.getInstance().getDrvr().findElement(email).sendKeys(password);
+	public void enterPassword(String passwordtxt) {
+		SingletonDriver.getInstance().getDrvr().findElement(password).sendKeys(passwordtxt);
 	}
-	public Homepage clickLogin() {
+	public void clickLogin() {
 		SingletonDriver.getInstance().getDrvr().findElement(btnLogin).click();
-		return new Homepage();
+		//return new Homepage();
+	}
+	@Override
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+		
+	}
+	@Override
+	public String getPageTitle() {
+		return this.pageTitle;
 	}
 }
