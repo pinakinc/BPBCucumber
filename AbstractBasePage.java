89f6c8813ca883ab2ecf454b756201197c03ae52
pinakinc <chaubalpinakin@gmail.com>
2020-09-07 17:23:26 +0530

    Update AbstractBasePage.java

--- AbstractBasePage.java
+++ AbstractBasePage.java
@@ -3,11 +3,24 @@ package pageobjects;
 import constants.GLOBAL_DATA;
 import singletondriver.SingletonDriver;
 
-public class AbstractBasePage {
-
-	public LandingPage navigateURL() {
+public abstract class AbstractBasePage {
+	protected String pageTitle; 
+	public void navigateURL() {
 		SingletonDriver.getInstance().getDrvr().navigate().to(GLOBAL_DATA.URL);
-		return new LandingPage();
+//		return new LandingPage();
 	}
+
+	public void openBrowser(String browserName, String platform, String gridSetting) {
+		try {
+			SingletonDriver.getInstance().setDrvr(browserName, platform, gridSetting, null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public abstract void setPageTitle(String pageTitle);
+	public abstract String getPageTitle();
 
 }
