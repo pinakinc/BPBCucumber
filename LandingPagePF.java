7d32f0047e24a8f0c64ab7ab2be1cc971a5cb4da
pinakinc <chaubalpinakin@gmail.com>
2020-09-09 11:18:24 +0530

    Create LandingPagePF.java

---
+++ LandingPagePF.java
@@ -0,0 +1,41 @@
+package pageobjectspf;
+
+
+import org.openqa.selenium.support.PageFactory;
+
+import locators.LandingPageLocators;
+import pageobjects.AbstractBasePage;
+import pageobjects.AbstractBasePagePF;
+import singletondriver.SingletonDriver;
+
+public class LandingPagePF extends AbstractBasePagePF{
+	
+	LandingPageLocators landingpageLocators;
+	
+	
+	
+	public LandingPagePF() {
+		super();
+		landingpageLocators = new LandingPageLocators();
+		PageFactory.initElements(SingletonDriver.getInstance().getDrvr(), landingpageLocators);
+		System.out.println("After init");
+	}
+
+	public void clickLogin() {
+		System.out.println("Before click"+this.landingpageLocators.btnLogin.getText());
+		this.landingpageLocators.btnLogin.click();
+		System.out.println("After click");
+	}
+
+	@Override
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle=pageTitle;
+		
+	}
+
+	@Override
+	public String getPageTitle() {
+		// TODO Auto-generated method stub
+		return this.pageTitle;
+	}
+}
