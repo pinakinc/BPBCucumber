2a84b1979d94edf4298f957980ff797d5214ea9b
pinakinc <chaubalpinakin@gmail.com>
2020-09-09 11:24:32 +0530

    Create LandingPageLocators.java

---
+++ LandingPageLocators.java
@@ -0,0 +1,14 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LandingPageLocators {
+
+	@FindBy(how=How.XPATH,using="//a[@href='https://ui.freecrm.com']")
+	public  WebElement btnLogin;
+	
+
+}
