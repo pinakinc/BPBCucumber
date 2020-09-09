4c082ea9aff104e8da85a274ee1806a7522517de
pinakinc <chaubalpinakin@gmail.com>
2020-09-09 11:25:36 +0530

    Create LoginPageLocators.java

---
+++ LoginPageLocators.java
@@ -0,0 +1,17 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginPageLocators {
+
+	@FindBy(name="email")
+	public WebElement email;
+	@FindBy(name="password")
+	public WebElement password;
+	@FindBy(how=How.XPATH,using="//div[text()='Login']")
+	public WebElement btnLogin;
+
+}
