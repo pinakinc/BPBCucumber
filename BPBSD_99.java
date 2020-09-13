69acd53d8bd5ae83b1e01e858ca66d8157b9ca56
pinakinc <chaubalpinakin@gmail.com>
2020-09-13 09:40:22 +0530

    Update BPBSD_99.java

--- BPBSD_99.java
+++ BPBSD_99.java
@@ -1,10 +1,17 @@
+package stepdefinitions;
+
+import cucumber.api.java.After;
+import locators.HomepageLocators;
+import singletondriver.SingletonDriver;
+
 public class BPBSD_99 {
+
 	@After
 	public void tearDown() {
-		System.out.println("After all methods");
-		SingletonDriver.getInstance().closeDriver();
+		System.out.println("In teardown");
+		SingletonDriver.getInstance().getDrvr().close();
+		SingletonDriver.getInstance().getDrvr().quit();
 
 	}
-
 
 }
