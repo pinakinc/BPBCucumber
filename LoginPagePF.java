ddaf689907dab14630d4c684f8466073f6a5f066
pinakinc <chaubalpinakin@gmail.com>
2020-09-20 18:27:44 +0530

    Update LoginPagePF.java

--- LoginPagePF.java
+++ LoginPagePF.java
@@ -1,11 +1,14 @@
 package pageobjectspf;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 
 import locators.LoginPageLocators;
 import singletondriver.SingletonDriver;
+import utils.LoggerUtil;
 
 public class LoginPagePF extends AbstractBasePagePF{
+	Logger logger = LoggerUtil.getCustomLogger(LoginPagePF.class);
 	LoginPageLocators loginpageLocators;
 	
 	public LoginPagePF() {
@@ -15,12 +18,15 @@ public class LoginPagePF extends AbstractBasePagePF{
 	}
 
 	public void enterUserName(String userName) {
+		logger.info("Entering username");
 		loginpageLocators.email.sendKeys(userName);
 	}
 	public void enterPassword(String passwordtxt) {
+		logger.info("Entering password");
 		loginpageLocators.password.sendKeys(passwordtxt);
 	}
 	public void clickLogin() {
+		logger.info("Clicking Login button on Login screen");
 		loginpageLocators.btnLogin.click();
 	}
 	@Override
