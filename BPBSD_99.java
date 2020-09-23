e6b384c85847b83376e938e89d9a2a9b71ce68bf
pinakinc <chaubalpinakin@gmail.com>
2020-09-23 15:17:41 +0530

    Update BPBSD_99.java

--- BPBSD_99.java
+++ BPBSD_99.java
@@ -1,16 +1,36 @@
 package stepdefinitions;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import locators.HomepageLocators;
+import pageobjects.AbstractBasePage;
 import singletondriver.SingletonDriver;
+import utils.Assertions;
 
 public class BPBSD_99 {
 
 	@After
-	public void tearDown() {
+	public void tearDown(Scenario scenario) {
 		System.out.println("In teardown");
-		SingletonDriver.getInstance().getDrvr().close();
-		SingletonDriver.getInstance().getDrvr().quit();
+		if (scenario.isFailed()) {
+			System.out.println("In teardown failure");
+			byte[] screenPrint = ((TakesScreenshot)SingletonDriver.getInstance().getDrvr()).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenPrint, "image/png");
+		}
+
+		try {
+			if (!(SingletonDriver.getInstance().getSessId()==null)) {
+			SingletonDriver.getInstance().getDrvr().close();
+			SingletonDriver.getInstance().getDrvr().quit();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		Assertions.assertAll();
 
 	}
 
