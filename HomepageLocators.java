ecd4dc779f6c987680ccab6ff34fd3695935591a
pinakinc <chaubalpinakin@gmail.com>
2020-09-23 16:36:40 +0530

    Update HomepageLocators.java

--- HomepageLocators.java
+++ HomepageLocators.java
@@ -1,15 +1,23 @@
 package locators;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class HomepageLocators {
 
+		
 	@FindBy(how=How.XPATH,using="//span[@class='user-display']")
 	public WebElement userDisplay;
+
+	@FindBy(how=How.XPATH,using="//a[@href=\"/settings/billing/\"]")
+	public WebElement acctType;
+
+	@CacheLookup
 	@FindBy(how=How.XPATH,using="//div[@role='listbox']")
 	public WebElement logOutDiv;
+	
 	@FindBy(how=How.XPATH,using="//span[text()='Log Out']")
 	public WebElement logOutSpan;
 	
