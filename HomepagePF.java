16bf8792d21f150747f7f74f38652ac83b3a569f
pinakinc <chaubalpinakin@gmail.com>
2020-09-23 16:34:34 +0530

    Update HomepagePF.java

--- HomepagePF.java
+++ HomepagePF.java
@@ -3,10 +3,14 @@ package pageobjectspf;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import locators.HomepageLocators;
 import singletondriver.SingletonDriver;
+import utils.Assertions;
 import utils.LoggerUtil;
 
 public class HomepagePF extends AbstractBasePagePF{
@@ -25,15 +29,23 @@ public class HomepagePF extends AbstractBasePagePF{
 	public HomepagePF verifyUser(String userDisplayText) {
 		logger.info("Verifying user text");
 		Assert.assertEquals(homepageLocators.userDisplay.getText(), userDisplayText);
+		//Assertions.assertEqualsSA(homepageLocators.userDisplay.getText(), userDisplayText);
 		return new HomepagePF();
 	}
-	
+
+	public HomepagePF verifyAccountType(String acctType) {
+		logger.info("Verifying account type");
+		Assert.assertEquals(homepageLocators.acctType.getText(), acctType);
+		//Assertions.assertEqualsSA(homepageLocators.userDisplay.getText(), userDisplayText);
+		return new HomepagePF();
+	}
+
 	public void logOut() {
 		logger.info("Clicking Logout Div");
 		homepageLocators.logOutDiv.click();
 		logger.info("Clicking Logout Span");
 		homepageLocators.logOutSpan.click();
-
+		
 	}
 
 	@Override
