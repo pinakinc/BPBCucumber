709a6576b6dca8b1ce08750e1f88f18a7b89d5e3
pinakinc <chaubalpinakin@gmail.com>
2020-09-27 08:52:29 +0530

    Update BPBSD_00.java

--- BPBSD_00.java
+++ BPBSD_00.java
@@ -1,5 +1,23 @@
+package stepdefinitions;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import constants.GLOBAL_DATA;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import singletondriver.SingletonDriver;
+import utils.CucumberCustomReporter;
+import utils.GenericHelper;
+
 public class BPBSD_00 {
-	@Given("^I open the \"([^\"]*)\" browser on \"([^\"]*)\" and \"([^\"]*)\"$")
+/*	@Given("^I open the \"([^\"]*)\" browser on \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void i_open_the_browser(String browserName, String platform, String gridSetting) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		try {
@@ -10,4 +28,36 @@ public class BPBSD_00 {
 		}
 
 	}
+*/
+	private static CucumberCustomReporter cucumberCustomReporter;
+	private static boolean isReportRunning;
+	private static GenericHelper genericHelper = new GenericHelper();
+	@Before
+	public void setUp(Scenario scenario) {
+		System.out.println("Before  any of the methods");
+		if(!isReportRunning) {
+			cucumberCustomReporter=new CucumberCustomReporter("D:\\Data\\Log\\TestReport.html");
+			isReportRunning = true;
+		}
+	}
+	
+	@After
+	public void tearDown(Scenario scenario) throws IOException {
+		System.out.println("After all methods");
+		File screenPrint = null;
+		String screenshotName = "D:\\Data\\Log\\"+scenario.getName().replaceAll(" ", "")+".jpeg";
+		System.out.println(screenshotName);
+		if (scenario.isFailed()){
+			//screenPrint=((TakesScreenshot)SingletonDriver.getInstance().getDrvr()).getScreenshotAs(OutputType.FILE);
+			genericHelper.takeScreenShot(screenshotName);
+			
+			
+		}
+//		FileUtils.copyFile(screenPrint, new File(screenshotName));
+		cucumberCustomReporter.createTest(scenario, screenshotName);
+		cucumberCustomReporter.writeReport();
+		
+	}
+
+
 }
