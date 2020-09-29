4d0b8f44de3c3b41aea9643c579e7cdc576d08e4
pinakinc <chaubalpinakin@gmail.com>
2020-09-29 19:26:59 +0530

    Update CucumberCustomReporter.java

--- src/main/java/utils/CucumberCustomReporter.java
+++ src/main/java/utils/CucumberCustomReporter.java
@@ -2,11 +2,15 @@ package utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import cucumber.api.Scenario;
+import gherkin.formatter.model.Result;
 
 public class CucumberCustomReporter {
 	private ExtentHtmlReporter extentHtmlReporter;
@@ -18,9 +22,10 @@ public class CucumberCustomReporter {
 		extentReports.attachReporter(extentHtmlReporter);
 	}
 	
-	public void createTest(Scenario scenario,String screenshotFile) throws IOException {
+	public void createCustomTest(Scenario scenario,String screenshotFile) throws IOException {
 		if (scenario!=null) {
 			String testName = getScenarioTitle(scenario);
+			String errMessage = fetchErrorMsg(scenario);
 			System.out.println("Status of scenario: "+scenario.getStatus());
 			System.out.println("Status of screenshotfile: "+screenshotFile);
 			switch(scenario.getStatus().toUpperCase()) {
@@ -29,7 +34,8 @@ public class CucumberCustomReporter {
 				break;
 			case "FAILED":
 				System.out.println("in failed");
-				extentReports.createTest(testName).fail("failed").addScreenCaptureFromPath(getImageLocation(screenshotFile));
+				//extentReports.createTest(testName).fail("failed").addScreenCaptureFromPath(getImageLocation(screenshotFile));
+				extentReports.createTest(testName).fail(errMessage).addScreenCaptureFromPath(screenshotFile);
 				break;
 			default:
 				extentReports.createTest(testName).fail("skipped");
@@ -37,6 +43,32 @@ public class CucumberCustomReporter {
 		}
 	}
 	
+	private String fetchErrorMsg(Scenario scenario) {
+		List<Result> testResults = null;
+		List<Result> failedResults = null;
+		try {
+			Field stepResults = scenario.getClass().getDeclaredField("stepResults");
+			//Bypass the security
+			stepResults.setAccessible(true);
+			testResults = (List<Result>)stepResults.get(scenario);
+		} catch (NoSuchFieldException|SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		if (testResults != null && !testResults.isEmpty()) {
+			//pick the results where error message is not null
+			failedResults = testResults.stream().filter((a) -> {
+				return a.getErrorMessage() != null;
+			}).collect(Collectors.toList());
+		}
+		
+		if (failedResults != null && !failedResults.isEmpty()) {
+			return failedResults.get(0).getErrorMessage();
+		}
+		return "";
+		
+	}
 	public void writeReport() {
 		if (extentReports!=null) {
 			extentReports.flush();
