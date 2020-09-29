250efd259f255369e46221b8892ed5a1cca8370b
pinakinc <chaubalpinakin@gmail.com>
2020-09-29 19:29:20 +0530

    Update BPBSD_99.java

--- src/test/java/stepdefinitions/BPBSD_99.java
+++ src/test/java/stepdefinitions/BPBSD_99.java
@@ -1,5 +1,8 @@
 package stepdefinitions;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -10,9 +13,9 @@ import pageobjects.AbstractBasePage;
 import singletondriver.SingletonDriver;
 import utils.Assertions;
 
-public class BPBSD_99 {
+public class BPBSD_99 extends BaseSteps{
 
-	@After
+	//@After
 	public void tearDown(Scenario scenario) {
 		System.out.println("In teardown");
 			if (scenario.isFailed()) {
@@ -34,5 +37,49 @@ public class BPBSD_99 {
 //		Assertions.assertAll();
 
 	}
+	
+	@After
+	public void tearDown1(Scenario scenario) throws IOException {
+		System.out.println("After all methods");
+		File screenPrint = null;
+		String screenshotName = "D:\\Data\\Log\\"+scenario.getName().replaceAll(" ", "")+".jpeg";
+		System.out.println(screenshotName);
+		if (scenario.isFailed()){
+			//screenPrint=((TakesScreenshot)SingletonDriver.getInstance().getDrvr()).getScreenshotAs(OutputType.FILE);
+			genericHelper.takeScreenShot(screenshotName);
+			
+			//Added on 28-SEPT-2020
+			byte[] screenPrint1 = ((TakesScreenshot)SingletonDriver.getInstance().getDrvr()).getScreenshotAs(OutputType.BYTES);
+			scenario.embed(screenPrint1, "image/png");
+			try {
+				if (!(SingletonDriver.getInstance().getSessId()==null)) {
+				SingletonDriver.getInstance().getDrvr().close();
+				SingletonDriver.getInstance().getDrvr().quit();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+			
+		} else {
+			try {
+				if (!(SingletonDriver.getInstance().getSessId()==null)) {
+				SingletonDriver.getInstance().getDrvr().close();
+				SingletonDriver.getInstance().getDrvr().quit();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//Added on 28-SEPT-2020
+		}
+//		FileUtils.copyFile(screenPrint, new File(screenshotName));
+		cucumberCustomReporter.createCustomTest(scenario, screenshotName);
+		cucumberCustomReporter.writeReport();
+		
+	}
+
 
 }
