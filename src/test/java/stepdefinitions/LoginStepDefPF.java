21520f4edbca6aaaee228ce801999ee39f3bf531
pinakinc <chaubalpinakin@gmail.com>
2020-10-03 10:00:55 +0530

    Update LoginStepDefPF.java

--- src/test/java/stepdefinitions/LoginStepDefPF.java
+++ src/test/java/stepdefinitions/LoginStepDefPF.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import pageobjects.LoginPage;
 import pageobjectspf.LoginPagePF;
+import utils.ExtentTstClass;
 
 public class LoginStepDefPF {
 	
@@ -11,18 +12,21 @@ public class LoginStepDefPF {
 	@When("^The user enters \"(.*?)\" as username$")
 	public void user_enters_admin_as_username(String userName) throws Throwable {
 		System.out.println("UserName is: "+userName);
+		ExtentTstClass.logInfo("UserName: "+userName);
 		loginPagePF.enterUserName(userName);
 	}
 
 	@And("^The user enters \"(.*?)\" as password$")
 	public void user_enters_admin_as_password(String password) throws Throwable {
 		System.out.println("Password is: "+password);
+		ExtentTstClass.logInfo("Password: "+password);
 		loginPagePF.enterPassword(password);
 	}
 
 	@And("^The user clicks the Login button$")
 	public void user_clicks_the_Login_button() throws Throwable {
 		System.out.println("Login button clicked");
+		ExtentTstClass.logInfo("Clicking Login");
 		loginPagePF.clickLogin();
 	}
 
